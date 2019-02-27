05303f37c5bfa9fa854ab4f611dec4b9552183e3
白露猪 <a123@>
2019-02-27 22:56:40 +0800

    Initial Commit

---
+++ ToyOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToyOne
+//
+//  Created by Antoine_Huai on 2019/2/27.
+//  Copyright © 2019年 Antoine_Huai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ToyOneTests/ToyOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToyOneTests.swift
+//  ToyOneTests
+//
+//  Created by Antoine_Huai on 2019/2/27.
+//  Copyright © 2019年 Antoine_Huai. All rights reserved.
+//
+
+import XCTest
+@testable import ToyOne
+
+class ToyOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
